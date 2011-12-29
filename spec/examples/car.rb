8844168290fa20a1a9efbee4ee8b1ee5514ccf2e
Federico Ravasio <ravasio.federico@gmail.com>
2011-12-29 17:57:32 +0100

    Introduced a 'complex' validation in tests.

--- spec/examples/car.rb
+++ spec/examples/car.rb
@@ -1,8 +1,8 @@
 class Car
   include Tyrion::Document
-  
+
   field :model
   field :plate
-  
-  validates :plate, :presence => true
+
+  validates :plate, :presence => true, :format => /^\w{2}\d{3}\w{2}$/i
 end
--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -9,7 +9,7 @@ end
 
 describe Tyrion::Validations do
   subject{ Car.new }
-  
+
   before(:each){ Car.reload }
 
   it 'should perform validations' do
@@ -17,14 +17,21 @@ describe Tyrion::Validations do
     subject.plate = 'AX567ED'
     should be_valid
   end
-  
+
+  it 'should perform complex validations' do
+    subject.plate = 'A'
+    should_not be_valid
+    subject.plate = 'AA000AA'
+    should be_valid
+  end
+
   it 'should say wether a document is new or not' do
     subject.plate = 'AX567ED'
     should be_a_new_document
     subject.save
     should_not be_a_new_document
   end
-  
+
   it 'should not save an invalid document' do
     subject.save.should be_false
     Car.all.count.should be_zero
